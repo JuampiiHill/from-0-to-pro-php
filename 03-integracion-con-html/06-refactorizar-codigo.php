ffb15fe9e6fbca8d6a0ad9c48ab416c14b53c26e
Juan Pablo Hillcoat <juanpablohillcoat@gmail.com>
2024-01-25 13:43:19 -0300

    fix spaguetti code

---
+++ 03-integracion-con-html/06-refactorizar-codigo.php
@@ -0,0 +1,35 @@
+<?php 
+
+$tabla_del_9 = [];
+
+for($i=1; $i <=10; $i++) {
+    $resultado = 9 * $i;
+    $texto = "9 x $i = $resultado";
+    array_push($tabla_del_9, $texto);
+}
+
+//die(); // die mata la pagina, nada de lo que siga debajo se va a cargar
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Refactorizando codigo</title>
+</head>
+<body>
+
+    <h1>Tabla del 9</h1>
+
+    <ul>
+
+    <?php foreach ($tabla_del_9 as $resultado): ?>
+        <li><?= $resultado?></li>
+    <?php endforeach; ?>
+
+    </ul>
+
+
+</body>
+</html>
